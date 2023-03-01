9dab9085785a12b99ccaca52bd7c827576a57fc7
Signal-flow12 <tcravenny@gmail.com>
2023-02-28 21:26:24 -0500

    added button

--- connect4.js
+++ connect4.js
@@ -1,3 +1,12 @@
+//event listeners for gameboard divs, score update, winning disk color, clear button.
+const squares = document.querySelectorAll('.square')
+//console.log(squares)
+const clearBoard = document.querySelectorAll()
 // players red disk and yellow disk
 
-//
+
+//red player starts first 
+
+
+
+//players have to take turns. 
